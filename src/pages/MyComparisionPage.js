013ecfe7a8ba585637a5ce006429d525980d45bc
Hyeongmin Jo <paengdal1@gmail.com>
2024-12-18 16:38:47 +0900

    feat: '기업 비교하기' 버튼, '기업 추가' 버튼, '추가된 기업 위젯' UI 구현

--- src/pages/MyComparisionPage.js
+++ src/pages/MyComparisionPage.js
@@ -10,12 +10,20 @@ function MyComparisionPage() {
       <Container>
         <div className="my-comparision-title">나의 기업을 선택해 주세요!</div>
         <div className="my-company-box">
-          <div className="add-button">
+          <div className="add-button-wrap">
             <div className="plus-icon">
               <img src={icPlus} alt="나의 기업선택" width="20px" />
             </div>
             <span>기업 추가</span>
           </div>
+          <div className="company-box">
+            <div className="company-image"></div>
+            <div className="company-box-name">코드잇</div>
+            <div className="company-box-category">에듀테크</div>
+          </div>
+        </div>
+        <div className="button-container">
+          <div className="primary-round-button disable">기업 비교하기</div>
         </div>
       </Container>
     </>
