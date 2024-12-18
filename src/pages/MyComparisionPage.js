b1fdf02809693e769ceca13289c3fdad22a124a9
Hyeongmin Jo <paengdal1@gmail.com>
2024-12-18 14:55:57 +0900

    feat: 기업 추가 박스 UI 구현

--- src/pages/MyComparisionPage.js
+++ src/pages/MyComparisionPage.js
@@ -1,12 +1,22 @@
 import Container from '../components/Container';
 import Header from '../components/Hearder';
+import './MyComparisionPage.css';
+import icPlus from '../assets/images/ic_plus.png';
 
 function MyComparisionPage() {
   return (
     <>
       <Header />
       <Container>
-        <div>나의 기업 비교</div>
+        <div className="my-comparision-title">나의 기업을 선택해 주세요!</div>
+        <div className="my-company-box">
+          <div className="add-button">
+            <div className="plus-icon">
+              <img src={icPlus} alt="나의 기업선택" width="20px" />
+            </div>
+            <span>기업 추가</span>
+          </div>
+        </div>
       </Container>
     </>
   );
