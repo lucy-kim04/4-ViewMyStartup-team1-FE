74a0057bcdd12fb91d3908ec26755895ef852c3c
Hyeongmin Jo <paengdal1@gmail.com>
2024-12-25 16:58:13 +0900

    fix: HeaderKSH.js의 comparison -> comparision

--- src/components/HeaderKSH.js
+++ src/components/HeaderKSH.js
@@ -9,8 +9,8 @@ export default function HeaderKSH() {
     <header className="header">
       <img src={imgLogo} alt="로고" width="112px" height="40px" />
       <div className="header-text">
-        <Link to="/my-comparison">나의 기업 비교</Link>
-        <Link to="/comparison-status">비교 현황</Link>
+        <Link to="/my-comparision">나의 기업 비교</Link>
+        <Link to="/comparision-status">비교 현황</Link>
         <Link to="/investment-status">투자 현황</Link>
       </div>
     </header>
